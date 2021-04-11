29fbe4a5bcf1533f4f754add6f80ecff0b65d7f7
sinirsizvesinifsiz <cihancan83@gmail.com>
2021-04-12 02:14:27 +0300

    12 Nisan

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController: ControllerBase
+    {
+        
+    }
+}
